3649b47dd0a1ca412459afcc856317f604504994
Chris van den Berg <cvandenberg1105@googlemail.com>
2024-08-30 00:43:41 +0100

    Fix formatting

--- seq2seq-expansion-strategy/src/models/utils.py
+++ seq2seq-expansion-strategy/src/models/utils.py
@@ -2,10 +2,12 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
+
 def preprocess_smiles(smiles_list, tokenizer, max_length):
     sequences = tokenizer.texts_to_sequences(smiles_list)
     padded_sequences = pad_sequences(sequences, maxlen=max_length, padding='post')
     return padded_sequences
+
 
 def create_tokenizer(smiles_list):
     tokenizer = tf.keras.preprocessing.text.Tokenizer(char_level=True, filters='')
