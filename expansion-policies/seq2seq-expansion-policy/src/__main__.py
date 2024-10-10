36a1d2e8f3d001484ab8ea5e09921f8cbb84cd16
Chris van den Berg <cvandenberg1105@googlemail.com>
2024-10-10 23:09:23 +0100

    Add logic to convert SavedModel format to ONNX format via tf2onnx CLI command

--- expansion-policies/seq2seq-expansion-policy/src/__main__.py
+++ expansion-policies/seq2seq-expansion-policy/src/__main__.py
@@ -1,14 +1,26 @@
+import os
+
+import yaml
 from trainers.trainer import Trainer
+from models.utils import Seq2SeqModelUtils
 
 def main():
     # Path to the configuration file
-    config_path = 'config/training/model_v16_config.yml'
+    config_path = 'config/training/model_v15_config.yml'
 
     # Initialize the Trainer with the configuration
     trainer = Trainer(config_path=config_path)
 
-    # Run the training pipeline
-    trainer.run()
+    config = trainer.load_config(config_path)
+    model_save_path = config['training']['model_save_path']
+    model_save_dir = config['training']['model_save_dir']
+
+    Seq2SeqModelUtils.convert_saved_model_to_hdf5(
+        model_save_path,
+        os.path.join(model_save_dir, 'hdf5')
+    )
+
+    test = 'test'
 
 if __name__ == '__main__':
     main()
--- expansion-policies/seq2seq-expansion-policy/src/models/utils.py
+++ expansion-policies/seq2seq-expansion-policy/src/models/utils.py
@@ -1,9 +1,20 @@
+import os
+import sys
+import subprocess
 from typing import List
 
-import tf2onnx
 import tensorflow as tf
+from tensorflow.keras import Model
 from sklearn.model_selection import ShuffleSplit, cross_validate
 
+from models.seq2seq import RetrosynthesisSeq2SeqModel
+from encoders.lstm_encoders import StackedBidirectionalLSTMEncoder
+from decoders.lstm_decoders import StackedLSTMDecoder
+from attention.attention import BahdanauAttention
+from losses.losses import MaskedSparseCategoricalCrossentropy
+from metrics.metrics import Perplexity
+from callbacks.checkpoints import BestValLossCallback
+from callbacks.bleu_score import BLEUScoreCallback
 
 class Seq2SeqModelUtils:
     @staticmethod
@@ -32,39 +43,63 @@ class Seq2SeqModelUtils:
         )
 
     @staticmethod
-    def inspect_model_layers(model) -> None:
+    def inspect_model_layers(model: Model) -> None:
         """
         Recursively inspect each layer and sublayer in the model and print their configuration.
         """
-        def _inspect_layer(layer, indent=0):
+        def _inspect_layer(model_layer, indent=0):
             indent_str = "  " * indent
-            print(f"{indent_str}Layer: {layer.name}")
-            config = layer.get_config()
+            print(f"{indent_str}Layer: {model_layer.name}")
+            config = model_layer.get_config()
             for key, value in config.items():
                 print(f"{indent_str}  - {key}: {value}")
 
             # Recursively inspect sublayers if any
-            if hasattr(layer, 'layers'):  # For layers like Bidirectional, TimeDistributed, etc.
-                for sublayer in layer.layers:
+            if hasattr(model_layer, 'layers'):  # For layers like Bidirectional, TimeDistributed, etc.
+                for sublayer in model_layer.layers:
                     _inspect_layer(sublayer, indent + 1)
-            elif hasattr(layer, 'layer'):  # For layers like RNN that contain a single layer
-                _inspect_layer(layer.layer, indent + 1)
+            elif hasattr(model_layer, 'layer'):  # For layers like RNN that contain a single layer
+                _inspect_layer(model_layer.layer, indent + 1)
 
         for layer in model.layers:
             _inspect_layer(layer)
 
     @staticmethod
-    def convert_to_onnx(saved_model_path, onnx_file_path) -> None:
-        """
-        Convert the TensorFlow SavedModel to ONNX format and save it.
-        """
-        # Load the TensorFlow model
-        model = tf.keras.models.load_model(saved_model_path)
+    def save_saved_model_format(model_save_path: str, model: Model) -> None:
+        os.makedirs(model_save_path, exist_ok=True)
+        model.export(model_save_path)
+        print(f"Model saved to {model_save_path}")
 
-        # Convert the TensorFlow model to ONNX format
-        onnx_model = tf2onnx.convert.from_keras(model)
+    @staticmethod
+    def convert_saved_model_to_onnx_cli(saved_model_path: str, onnx_output_dir: str) -> None:
+        """
+        Convert SavedModel to ONNX format.
 
-        # Save the ONNX model to a file
-        with open(onnx_file_path, "wb") as f:
-            f.write(onnx_model.SerializeToString())
-        print(f"ONNX model saved to {onnx_file_path}")
+        Conversion of SavedModel format via tf2onnx Python API has been deprecated. However, conversion to ONNX via CLI
+        is still supported.
+
+        :param saved_model_path:
+        :param onnx_output_dir:
+        :return:
+        """
+        os.makedirs(onnx_output_dir, exist_ok=True)
+        onnx_output_path = os.path.join(onnx_output_dir, "seq2seq_model.onnx")
+
+        tf2onnx_cli_command = [
+            sys.executable, '-m', 'tf2onnx.convert',
+            '--saved-model', saved_model_path,
+            '--output', onnx_output_path,
+            '--opset', '13',
+        ]
+
+        result = subprocess.run(
+            tf2onnx_cli_command,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True
+        )
+
+        if result.returncode == 0:
+            print(f"Model has been converted to ONNX format at {onnx_output_path}")
+        else:
+            print(f"Error converting model to ONNX:\n{result.stderr}")
