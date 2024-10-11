6c1094d01dd52ba2a08ab84849fd91975f4dd0b1
Chris van den Berg <cvandenberg1105@googlemail.com>
2024-10-11 18:32:41 +0100

    Update config and main in preparation for training run

--- expansion-policies/seq2seq-expansion-policy/src/__main__.py
+++ expansion-policies/seq2seq-expansion-policy/src/__main__.py
@@ -5,7 +5,7 @@ from trainers.trainer import Trainer
 
 def main():
     # Path to the configuration file
-    config_path = 'config/training/model_v18_config.yml'
+    config_path = 'config/training/model_v17_config.yml'
 
     # Initialize the Trainer with the configuration
     trainer = Trainer(config_path=config_path)
