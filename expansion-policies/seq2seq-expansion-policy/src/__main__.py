05d0eaba6c78aec25d1c2c7854b16c796bcb5388
Chris van den Berg <cvandenberg1105@googlemail.com>
2024-11-27 00:26:15 +0000

    Prepare config.xml and main.py for Model V28 training run

--- expansion-policies/seq2seq-expansion-policy/src/__main__.py
+++ expansion-policies/seq2seq-expansion-policy/src/__main__.py
@@ -6,13 +6,10 @@ import tensorflow as tf
 from trainers.trainer import Trainer
 
 def main():
-    # Path to the configuration file
-    config_path = 'config/training/model_v27_config.yml'
+    config_path = 'config/training/model_v28_config.yml'
 
-    # Initialize the Trainer with the configuration
     trainer = Trainer(config_path=config_path)
 
-    # Run the training pipeline
     trainer.run()
 
 if __name__ == "__main__":
