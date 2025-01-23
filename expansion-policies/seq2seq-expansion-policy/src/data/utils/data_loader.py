Fix bugs in new WeightedSparseCategoricalCrossEntropy logic, make all DataLoader attributes private and only accessible via properties, and add logic to use Square Root Weighting and Log Weighting in build_token_weight_map() method. Also add new configs to change max token value, and to control whether to use weighted loss

expansion-policies/seq2seq-expansion-policy/src/data/utils/data_loader.py
@@ -54,66 +54,58 @@ class DataLoader:
         batch_size: int = DEFAULT_BATCH_SIZE,
         buffer_size: int = DEFAULT_BUFFER_SIZE,
         random_state: int = DEFAULT_RANDOM_STATE,
+        max_tokens: int = DEFAULT_MAX_TOKENS,
         reverse_input_sequence: bool = DEFAULT_REVERSE_INPUT_SEQ_BOOL
     ) -> None:
-        self.products_file = products_file
-        self.reactants_file = reactants_file
-        self.test_split = test_split
-        self.validation_split = validation_split
+        self._products_file = products_file
+        self._reactants_file = reactants_file
+        self._test_split = test_split
+        self._validation_split = validation_split
         self._logger = logger
-        self.num_samples = num_samples
-        self.max_encoder_seq_length = max_encoder_seq_length
-        self.max_decoder_seq_length = max_decoder_seq_length
-        self.batch_size = batch_size
-        self.buffer_size = buffer_size
-        self.random_state = random_state
+        self._num_samples = num_samples
+        self._max_encoder_seq_length = max_encoder_seq_length
+        self._max_decoder_seq_length = max_decoder_seq_length
+        self._batch_size = batch_size
+        self._buffer_size = buffer_size
+        self._random_state = random_state
+        self._max_tokens = max_tokens
 
-        total_split = self.test_split + self.validation_split
-        if not (0 < self.test_split < 1 and 0 < self.validation_split < 1):
+        total_split = self._test_split + self._validation_split
+        if not (0 < self._test_split < 1 and 0 < self._validation_split < 1):
             raise ValueError("test_split and validation_split must be between 0 and 1.")
         if not (0 < total_split < 1):
             raise ValueError("The sum of test_split and validation_split must be between 0 and 1.")
 
-        self.train_split = 1.0 - total_split
+        self._train_split = 1.0 - total_split
 
         self._smiles_tokeniser = SmilesTokeniser(
             logger=self._logger,
             reverse_input_sequence=reverse_input_sequence
         )
 
-        self.encoder_data_processor = None
-        self.decoder_data_processor = None
+        self._encoder_data_processor = None
+        self._decoder_data_processor = None
 
-        self.train_data = None
-        self.valid_data = None
-        self.test_data = None
-        self.test_dataset_size = None
+        self._products_x_dataset = None
+        self._reactants_y_dataset = None
 
-        self.token_counts = None
+        self._tokenised_products_x_dataset = None
+        self._tokenised_reactants_y_dataset = None
 
-    @property
-    def vocab_size(self) -> int:
-        """
-        Returns the size of the tokeniser's vocabulary.
+        self._tokenised_products_x_train_data = None
+        self._tokenised_reactants_y_train_data = None
+        self._tokenised_products_x_valid_data = None
+        self._tokenised_reactants_y_valid_data = None
+        self._tokenised_products_x_test_data = None
+        self._tokenised_reactants_y_test_data = None
 
-        Returns
-        -------
-        int
-            Vocabulary size.
-        """
-        return self.smiles_tokeniser.vocab_size
+        self._test_size = None
+        self._train_data = None
+        self._valid_data = None
+        self._test_data = None
+        self._test_dataset_size = None
 
-    @property
-    def smiles_tokeniser(self) -> 'SmilesTokeniser':
-        """
-        Returns the SMILES tokeniser instance.
-
-        Returns
-        -------
-        SmilesTokeniser
-            The SMILES tokeniser used for tokenising SMILES strings.
-        """
-        return self._smiles_tokeniser
+        self._token_counts = None
 
     def load_and_prepare_data(self) -> None:
         """
@@ -152,25 +144,25 @@ class DataLoader:
         ValueError
             If there is a mismatch in the lengths of the datasets.
         """
-        self.products_x_dataset: List[str] = load_smiles_from_file(self.products_file)
-        self.reactants_y_dataset: List[str] = load_smiles_from_file(self.reactants_file)
+        self._products_x_dataset: List[str] = load_smiles_from_file(self._products_file)
+        self._reactants_y_dataset: List[str] = load_smiles_from_file(self._reactants_file)
 
         # Ensure datasets have the same length
-        if len(self.products_x_dataset) != len(self.reactants_y_dataset):
+        if len(self._products_x_dataset) != len(self._reactants_y_dataset):
             raise ValueError("Mismatch in dataset lengths.")
 
         # Limit the number of samples if specified
-        if self.num_samples is not None:
-            self.products_x_dataset = self.products_x_dataset[:self.num_samples]
-            self.reactants_y_dataset = self.reactants_y_dataset[:self.num_samples]
+        if self._num_samples is not None:
+            self._products_x_dataset = self._products_x_dataset[:self._num_samples]
+            self._reactants_y_dataset = self._reactants_y_dataset[:self._num_samples]
 
         # Canonicalise SMILES strings
         smiles_preprocessor: SmilesDataPreprocessor = SmilesDataPreprocessor()
-        self.products_x_dataset = [
-            smiles_preprocessor.canonicalise_smiles(smi) for smi in self.products_x_dataset
+        self._products_x_dataset = [
+            smiles_preprocessor.canonicalise_smiles(smi) for smi in self._products_x_dataset
         ]
-        self.reactants_y_dataset = [
-            smiles_preprocessor.canonicalise_smiles(smi) for smi in self.reactants_y_dataset
+        self._reactants_y_dataset = [
+            smiles_preprocessor.canonicalise_smiles(smi) for smi in self._reactants_y_dataset
         ]
 
     def _tokenise_datasets(self) -> None:
@@ -182,12 +174,12 @@ class DataLoader:
         -------
             None
         """
-        self.tokenised_products_x_dataset: List[str] = self.smiles_tokeniser.tokenise_list(
-            self.products_x_dataset,
+        self._tokenised_products_x_dataset: List[str] = self._smiles_tokeniser.tokenise_list(
+            self._products_x_dataset,
             is_input_sequence=True
         )
-        self.tokenised_reactants_y_dataset = self.smiles_tokeniser.tokenise_list(
-            self.reactants_y_dataset,
+        self._tokenised_reactants_y_dataset = self._smiles_tokeniser.tokenise_list(
+            self._reactants_y_dataset,
             is_input_sequence=False
         )
 
@@ -210,37 +202,37 @@ class DataLoader:
             If the tokenised datasets are empty or invalid.
         """
         # First split: Train vs Temp (Test + Validation)
-        (self.tokenised_products_x_train_data, self.tokenised_products_x_temp_data,
-         self.tokenised_reactants_y_train_data, self.tokenised_reactants_y_temp_data) = train_test_split(
-            self.tokenised_products_x_dataset,
-            self.tokenised_reactants_y_dataset,
-            test_size=(self.test_split + self.validation_split),
-            random_state=self.random_state,
+        (self._tokenised_products_x_train_data, tokenised_products_x_temp_data,
+         self._tokenised_reactants_y_train_data, tokenised_reactants_y_temp_data) = train_test_split(
+            self._tokenised_products_x_dataset,
+            self._tokenised_reactants_y_dataset,
+            test_size=(self._test_split + self._validation_split),
+            random_state=self._random_state,
             shuffle=True
         )
 
         # Calculate the proportion of validation split relative to the temp data
-        validation_ratio = self.validation_split / (self.test_split + self.validation_split)
+        validation_ratio = self._validation_split / (self._test_split + self._validation_split)
 
         # Second split: Validation vs Test from Temp
-        (self.tokenised_products_x_valid_data, self.tokenised_products_x_test_data,
-         self.tokenised_reactants_y_valid_data, self.tokenised_reactants_y_test_data) = train_test_split(
-            self.tokenised_products_x_temp_data,
-            self.tokenised_reactants_y_temp_data,
+        (self._tokenised_products_x_valid_data, self._tokenised_products_x_test_data,
+         self._tokenised_reactants_y_valid_data, self._tokenised_reactants_y_test_data) = train_test_split(
+            tokenised_products_x_temp_data,
+            tokenised_reactants_y_temp_data,
             test_size=(1-validation_ratio),
-            random_state=self.random_state,
+            random_state=self._random_state,
             shuffle=True
         )
 
         # Store test dataset size for partial test evaluation
-        self.test_size = len(self.tokenised_products_x_test_data)
+        self._test_size = len(self._tokenised_products_x_test_data)
 
         # Adapt the tokeniser on the training data only
-        combined_tokenised_train_data = self.tokenised_products_x_train_data + self.tokenised_reactants_y_train_data
-        self.smiles_tokeniser.adapt(combined_tokenised_train_data)
+        combined_tokenised_train_data = self._tokenised_products_x_train_data + self._tokenised_reactants_y_train_data
+        self._smiles_tokeniser.adapt(combined_tokenised_train_data)
 
         # Extract token frequencies
-        self.token_counts = self._smiles_tokeniser.get_token_frequencies(combined_tokenised_train_data)
+        self._smiles_tokeniser.calculate_token_frequencies(combined_tokenised_train_data)
 
     def _preprocess_tokenised_datasets(self) -> None:
         """
@@ -255,35 +247,36 @@ class DataLoader:
         None
         """
         # Initialise DataPreprocessors
-        self.encoder_data_processor = TokenisedSmilesPreprocessor(
-            self.smiles_tokeniser,
-            self.max_encoder_seq_length
+        self._encoder_data_processor = TokenisedSmilesPreprocessor(
+            self._smiles_tokeniser,
+            self._max_encoder_seq_length
         )
-        self.decoder_data_processor = TokenisedSmilesPreprocessor(
-            self.smiles_tokeniser,
-            self.max_decoder_seq_length
+        self._decoder_data_processor = TokenisedSmilesPreprocessor(
+            self._smiles_tokeniser,
+            self._max_decoder_seq_length
         )
 
         # Preprocess training data
-        self.train_data = self._preprocess_data_pair(
-            self.tokenised_products_x_train_data,
-            self.tokenised_reactants_y_train_data
+        self._train_data = self._preprocess_data_pair(
+            self._tokenised_products_x_train_data,
+            self._tokenised_reactants_y_train_data
         )
 
         # Preprocess test data
-        self.test_data = self._preprocess_data_pair(
-            self.tokenised_products_x_test_data,
-            self.tokenised_reactants_y_test_data
+        self._test_data = self._preprocess_data_pair(
+            self._tokenised_products_x_test_data,
+            self._tokenised_reactants_y_test_data
         )
 
         # Preprocess validation data
-        self.valid_data = self._preprocess_data_pair(
-            self.tokenised_products_x_valid_data,
-            self.tokenised_reactants_y_valid_data
+        self._valid_data = self._preprocess_data_pair(
+            self._tokenised_products_x_valid_data,
+            self._tokenised_reactants_y_valid_data
         )
 
     def _preprocess_data_pair(
-        self,encoder_data: List[str],
+        self,
+        encoder_data: List[str],
         decoder_data: List[str]
     ) -> Tuple[Tuple[tf.Tensor, tf.Tensor], tf.Tensor]:
         """
@@ -305,8 +298,8 @@ class DataLoader:
             - decoder_target : tf.Tensor
                 Decoder target tensor.
         """
-        encoder_input = self.encoder_data_processor.preprocess_smiles(encoder_data)
-        decoder_full = self.decoder_data_processor.preprocess_smiles(decoder_data)
+        encoder_input = self._encoder_data_processor.preprocess_smiles(encoder_data)
+        decoder_full = self._decoder_data_processor.preprocess_smiles(decoder_data)
         decoder_input = decoder_full[:, :-1]
         decoder_target = decoder_full[:, 1:]
         return (encoder_input, decoder_input), decoder_target
@@ -343,10 +336,10 @@ class DataLoader:
         ))
 
         if training:
-            dataset = dataset.shuffle(self.buffer_size)
+            dataset = dataset.shuffle(self._buffer_size)
 
-        # Use drop_remainder=True during training to ensure consistent batch sizes
-        dataset = dataset.batch(self.batch_size, drop_remainder=training)
+        # Use `drop_remainder=True` during training to ensure consistent batch sizes
+        dataset = dataset.batch(self._batch_size, drop_remainder=training)
         dataset = dataset.prefetch(tf.data.AUTOTUNE)
         return dataset
 
@@ -361,10 +354,10 @@ class DataLoader:
         tf.data.Dataset
             The training dataset.
         """
-        if self.train_data is None:
+        if self._train_data is None:
             raise ValueError("Training data has not been loaded and preprocessed.")
 
-        return self.get_dataset(self.train_data, training=True)
+        return self.get_dataset(self._train_data, training=True)
 
     def get_valid_dataset(self) -> tf.data.Dataset:
         """
@@ -377,10 +370,10 @@ class DataLoader:
         tf.data.Dataset
             The validation dataset.
         """
-        if self.valid_data is None:
+        if self._valid_data is None:
             raise ValueError("Validation data has not been loaded and preprocessed.")
 
-        return self.get_dataset(self.valid_data, training=False)
+        return self.get_dataset(self._valid_data, training=False)
 
     def get_test_dataset(self) -> tf.data.Dataset:
         """
@@ -393,7 +386,31 @@ class DataLoader:
         tf.data.Dataset
             The test dataset.
         """
-        if self.test_data is None:
+        if self._test_data is None:
             raise ValueError("Test data has not been loaded and preprocessed.")
 
-        return self.get_dataset(self.test_data, training=False)
+        return self.get_dataset(self._test_data, training=False)
+
+    @property
+    def vocab_size(self) -> int:
+        """
+        Returns the size of the tokeniser's vocabulary.
+
+        Returns
+        -------
+        int
+            Vocabulary size.
+        """
+        return self._smiles_tokeniser.vocab_size
+
+    @property
+    def smiles_tokeniser(self) -> 'SmilesTokeniser':
+        """
+        Returns the SMILES tokeniser instance.
+
+        Returns
+        -------
+        SmilesTokeniser
+            The SMILES tokeniser used for tokenising SMILES strings.
+        """
+        return self._smiles_tokeniser

expansion-policies/seq2seq-expansion-policy/src/data/utils/tokenisation.py
@@ -1,5 +1,6 @@
 import logging
 import json
+import math
 from collections import Counter
 from typing import Dict, List, Union
 
@@ -58,6 +59,7 @@ class SmilesTokeniser:
         self._start_token = start_token
         self._end_token = end_token
         self._oov_token = oov_token
+        self._max_tokens = max_tokens
         self.reverse_input_sequence = reverse_input_sequence
 
         # Initialize TextVectorization layer
@@ -70,10 +72,13 @@ class SmilesTokeniser:
             pad_to_max_tokens=False,
         )
 
+        self._token_counts = None
+
     @classmethod
     def from_json(
         cls,
         tokenizer_path: str,
+        logger: logging.Logger,
         reverse_input_sequence: bool = False,
         start_token: str = '<START>',
         end_token: str = '<END>',
@@ -105,6 +110,7 @@ class SmilesTokeniser:
 
         # Initialize a new SmilesTokenizer instance
         smiles_tokenizer = cls(
+            logger=logger,
             start_token=start_token,
             end_token=end_token,
             oov_token=oov_token,
@@ -242,7 +248,7 @@ class SmilesTokeniser:
             texts.append(' '.join(tokens))
         return texts
 
-    def get_token_frequencies(self, tokenized_smiles_list) -> Dict:
+    def calculate_token_frequencies(self, tokenized_smiles_list) -> Dict:
         token_counts = Counter()
         for line in tokenized_smiles_list:
             tokens = line.split()
@@ -257,13 +263,13 @@ class SmilesTokeniser:
             percentage = count / total_count * 100
             self._logger.info(f"Token: {token:10s} | Count: {count:7d} | {percentage:6.2f}%")
 
-        return dict(token_counts)
+        self._token_counts = dict(token_counts)
 
     def build_token_weight_map(
         self,
         token_counts: Dict,
-        alpha: float = 1.0,
-        min_count: int = 1
+        alpha: float = 0.1,
+        min_count: int = 10
     ):
         """
         Build a 1D float32 tensor of shape [vocab_size],
@@ -284,42 +290,59 @@ class SmilesTokeniser:
             freq = token_counts.get(token_str, 1)
             freq = max(freq, min_count)  # to avoid divide by zero
 
-            weights_array[idx] = alpha / float(freq)
+            # Square root weighting
+            ## weights_array[idx] = alpha / math.sqrt(freq)
+
+            # Log Weighting
+            weights_array[idx] = alpha / math.log(float(freq) + math.e)
 
         return tf.constant(weights_array, dtype=tf.float32)
 
     @property
-    def start_token(self):
+    def max_tokens(self) -> int:
+        """
+        Returns the tokeniser vocabulary maximum number of tokens.
+
+        Returns
+        -------
+        max_tokens: int
+            The start token.
+        """
+
+        return self._max_tokens
+
+    @property
+    def start_token(self) -> str:
         """
         Returns the start token used in tokenization.
 
         Returns
         -------
-        str
+        start_token: str
             The start token.
         """
         return self._start_token
 
     @property
-    def end_token(self):
+    def end_token(self) -> str:
         """
         Returns the end token used in tokenization.
 
         Returns
         -------
-        str
+        end_token: str
             The end token.
         """
         return self._end_token
 
     @property
-    def oov_token(self):
+    def oov_token(self) -> str:
         """
         Returns the out-of-vocabulary (OOV) token used in tokenization.
 
         Returns
         -------
-        str
+        oov_token: str
             The OOV token.
         """
         return self._oov_token
@@ -331,20 +354,32 @@ class SmilesTokeniser:
 
         Returns
         -------
-        dict
+        word_index: dict
             Word to index mapping.
         """
         vocab = self.text_vectorization.get_vocabulary()
         return {word: idx for idx, word in enumerate(vocab)}
 
     @property
-    def vocab_size(self):
+    def vocab_size(self) -> int:
         """
         Gets the size of the vocabulary.
 
         Returns
         -------
-        int
+        vocab_size: int
             Vocabulary size.
         """
         return len(self.text_vectorization.get_vocabulary())
+
+    @property
+    def token_counts(self) -> Dict:
+        """
+        Gets the counts for each token in the vocabulary.
+
+        Returns
+        -------
+        token_counts: Dict
+            Token counts.
+        """
+        return self._token_counts

expansion-policies/seq2seq-expansion-policy/src/losses/losses.py
@@ -7,7 +7,12 @@ from tensorflow.keras.losses import Loss
 class WeightedSparseCategoricalCrossEntropy(Loss):
     """
     A custom Weighted Sparse Categorical Crossentropy class.
+
     This loss applies a per-token weight factor and masks out padding tokens.
+
+    Padding tokens are used in sequence modeling tasks to ensure uniform input lengths. By masking these tokens,
+    the loss calculation focuses only on meaningful parts of the sequences, improving training efficiency and
+    performance.
 
     Parameters
     ----------
@@ -41,24 +46,57 @@ class WeightedSparseCategoricalCrossEntropy(Loss):
 
     def call(self, y_true, y_pred):
         """
-        y_true: (batch_size, seq_length)
-        y_pred: (batch_size, seq_length, vocab_size)
+        Computes the masked sparse categorical cross-entropy loss.
+
+        Parameters
+        ----------
+        y_true : tf.Tensor
+            Ground truth token tensor of shape `(batch_size, sequence_length)`.
+            Each entry should be an integer representing the correct token class.
+        y_pred : tf.Tensor
+            Predicted token tensor of shape `(batch_size, sequence_length, vocab_size)`.
+            Represents the probability distribution (float) over token classes for each timestep.
+
+        Returns
+        -------
+        tf.Tensor
+            Scalar tensor representing the mean loss over non-padding tokens.
+
+        Raises
+        ------
+        ValueError
+            If `y_true` is not a 2D tensor or `y_pred` is not a 3D tensor.
         """
-        # 1) Compute raw per-token crossentropy, shape = (batch_size, seq_length)
+        # 1) Validate input dimensions
+        if y_true.ndim != 2:
+            raise ValueError(f"y_true must be a 2D tensor, got {y_true.ndim}D tensor.")
+        if y_pred.ndim != 3:
+            raise ValueError(f"y_pred must be a 3D tensor, got {y_pred.ndim}D tensor.")
+
+        # 2) Defensive logic to cast `y_true` to int32 to ensure tf.gather can handle it
+        y_true = tf.cast(y_true, tf.int32)
+
+        # 3) Compute raw per-token crossentropy, shape = (batch_size, seq_length)
         per_token_loss = self.loss(y_true, y_pred)
 
-        # 2) Gather weights for each token in y_true
+        # 4) Gather weights for each token in y_true
         weights = tf.gather(self.token_to_weight_map, y_true)  # shape: (batch, seq_len)
 
-        # 3) Multiply: Multiply raw loss by these weights
-        weighted_loss = per_token_loss * weights
-
-        # 4) Mask out pad tokens (zero their loss contribution)
+        # 5) Mask out pad tokens (zero their loss contribution)
         mask = tf.cast(tf.not_equal(y_true, self.pad_token_id), tf.float32)
-        weighted_loss *= mask
 
-        # 5) Reduce to a single scalar (average over all non-padding tokens)
-        return tf.reduce_mean(weighted_loss)
+        # 6) Multiply raw loss for each token by weights to give weighted loss for each token, and then mask out
+        #    padding tokens (zero their loss contribution)
+        weighted_per_token_loss = per_token_loss * weights * mask
+
+        # 7) Sum the weighted losses
+        total_loss = tf.reduce_sum(weighted_per_token_loss)
+
+        # 8) Sum the effective weights
+        total_weight = tf.reduce_sum(weights * mask) + 1e-7  # small epsilon to avoid /0
+
+        # 9) Return average weighted loss
+        return total_loss / total_weight
 
 
 @tf.keras.utils.register_keras_serializable()
@@ -126,11 +164,11 @@ class MaskedSparseCategoricalCrossEntropy(Loss):
         Parameters
         ----------
         y_true : tf.Tensor
-            Ground truth tensor of shape `(batch_size, sequence_length)`.
-            Each entry should be an integer representing the correct class.
+            Ground truth token tensor of shape `(batch_size, sequence_length)`.
+            Each entry should be an integer representing the correct token class.
         y_pred : tf.Tensor
-            Predicted tensor of shape `(batch_size, sequence_length, vocab_size)`.
-            Represents the probability distribution over classes for each timestep.
+            Predicted token tensor of shape `(batch_size, sequence_length, vocab_size)`.
+            Represents the probability distribution (float) over token classes for each timestep.
 
         Returns
         -------

expansion-policies/seq2seq-expansion-policy/src/trainers/trainer.py
@@ -133,7 +133,8 @@ class Trainer:
             max_decoder_seq_length=data_conf.get('max_decoder_seq_length', 140),
             batch_size=data_conf.get('batch_size', 16),
             random_state=data_conf.get('random_state', 42),
-            reverse_input_sequence=train_conf.get('reverse_tokenized_input_sequence', True),
+            max_tokens = data_conf.get('max_tokens', 150),
+            reverse_input_sequence=train_conf.get('reverse_tokenized_input_sequence', True)
         )
 
         # Load and prepare data
@@ -197,6 +198,7 @@ class Trainer:
             If required model configuration keys are missing.
         """
         model_conf: Dict[str, Any] = self.config.get('model', {})
+        data_conf: Dict[str, Any] = self.config.get('data', {})
 
         # Retrieve model parameters with defaults
         encoder_embedding_dim: int = model_conf.get('encoder_embedding_dim', 256)
@@ -227,19 +229,30 @@ class Trainer:
         # Set up the optimiser
         self.optimizer: Adam = Adam(learning_rate=learning_rate, clipnorm=5.0)
 
-        token_to_weight_map = self.tokeniser.build_token_weight_map(
-            token_counts=self.data_loader.token_counts
-        )
-
-        self.loss_function = WeightedSparseCategoricalCrossEntropy(
-            token_to_weight_map=token_to_weight_map,
-            from_logits=False
-        )
-
         # Set up the loss function and metrics.
-        # For accuracy, because our sequences are integer-encoded, we have to specify `SparseCategoricalAccuracy`
-        # (Kera's default accuracy is CategoricalAccuracy, which is for sequences that are one-hot encoded)
-        self.loss_function = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False)
+        # For loss:
+        #   - If `use_weighted_loss` config is `True`, we build a token-to-weight map where a tokens weight is
+        #     dictated by its frequency in the training dataset. These weights are then applied to each token class in
+        #     a custom `WeightedSparseCategoricalCrossEntropy`.
+        #   - If `use_weighted_loss` config is `False`, we simply use the core
+        #     `tf.keras.losses.SparseCategoricalCrossentropy()` method.
+        #
+        # For accuracy:
+        #   - Because our sequences are integer-encoded, we have to specify `SparseCategoricalAccuracy`
+        #     (Kera's default accuracy is CategoricalAccuracy, which is for sequences that are one-hot encoded)
+        use_weighted_loss: bool = data_conf.get('use_weighted_loss', False)
+        if use_weighted_loss:
+            token_to_weight_map = self.tokeniser.build_token_weight_map(
+                token_counts=self.tokeniser.token_counts
+            )
+
+            self.loss_function = WeightedSparseCategoricalCrossEntropy(
+                token_to_weight_map=token_to_weight_map,
+                from_logits=False
+            )
+        else:
+            self.loss_function = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False)
+
         self.metrics = [
             tf.keras.metrics.SparseCategoricalAccuracy(name="accuracy"),
             Perplexity(loss_function=self.loss_function)
@@ -341,7 +354,7 @@ class Trainer:
             validation_metrics_dir=valid_metrics_dir,
             tensorboard_dir=os.path.join(tensorboard_dir, 'validation_metrics'),
             logger=self._logger,
-            max_length=self.data_loader.max_decoder_seq_length
+            max_length=self.data_loader._max_decoder_seq_length
         )
 
         # TensorBoard
@@ -431,7 +444,7 @@ class Trainer:
             # Shuffle and take the first `partial_count` samples
             test_dataset = (
                 test_dataset
-                .shuffle(buffer_size=test_dataset_size, seed=self.data_loader.random_state)
+                .shuffle(buffer_size=test_dataset_size, seed=self.data_loader._random_state)
                 .take(partial_count)
             )
 
@@ -462,7 +475,7 @@ class Trainer:
             predicted_sequences_list, predicted_scores_list = self.model.predict_sequence_beam_search(
                 encoder_input=encoder_input,
                 beam_width=model_conf.get('beam_width', 5),
-                max_length=self.data_loader.max_decoder_seq_length,
+                max_length=self.data_loader._max_decoder_seq_length,
                 start_token_id=self.tokeniser.word_index.get(start_token),
                 end_token_id=self.tokeniser.word_index.get(end_token),
                 return_top_n=1